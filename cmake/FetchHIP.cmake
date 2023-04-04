023be91253e10dde7aaa577fe26d3136e2f37c31
neoblizz <osama94@gmail.com>
2023-04-04 10:35:39 -0700

    Bug fixed.

--- cmake/FetchHIP.cmake
+++ cmake/FetchHIP.cmake
@@ -1,1 +1,4 @@
-find_package(HIP REQUIRED)
+# Find HIP module, this allows us to link to hip::device (gpu)
+# and hip::host (non-gpu) as needed.
+find_package(hip REQUIRED CONFIG PATHS ${HIP_PATH} ${ROCM_PATH})
+find_package(rocprim REQUIRED CONFIG PATHS ${HIP_PATH} ${ROCM_PATH})
